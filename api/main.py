958e38ff0004da40072a0388ad57a317fed2bd77
PacoLunaMX <f.hinojosa9@hotmail.com>
2023-12-04 08:49:32 -0600

    add logging config

--- api/main.py
+++ api/main.py
@@ -3,11 +3,13 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from api.database import database
+from api.logging_conf import configure_logging
 from api.routers.post import router as post_router
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    configure_logging()
     await database.connect()
     yield
     await database.disconnect()
